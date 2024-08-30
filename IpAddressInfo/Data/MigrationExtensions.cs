5ceb6b5a2f2910312734667e63edbfaaa8122b94
aris <kotsias.a@gmail.com>
2024-08-30 14:40:23 +0200

    fix docker-compose.yml and apply migrations on startup

--- IpAddressInfo/Data/MigrationExtensions.cs
+++ IpAddressInfo/Data/MigrationExtensions.cs
@@ -2,12 +2,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IpAddressInfo.Data;
 
-public static class MigrationExtensions
+public static class ApplicationBuilderExtensions
 {
     public static void ApplyMigrations(this IApplicationBuilder app)
     {
         using var scope = app.ApplicationServices.CreateScope();
-        using AppDbContext context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        var dbContextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
+        
+        using var context = dbContextFactory.CreateDbContext();
         context.Database.Migrate();
     }
 }
